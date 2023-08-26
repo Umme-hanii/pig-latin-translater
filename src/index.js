4d893f5b2262b0807ad28a59343823fc30bb219a
Umme-hanii <ummehanimohammed9@gmail.com>
2023-08-26 23:01:36 +0530

    Translate sentence to pig latin

--- src/index.js
+++ src/index.js
@@ -1,36 +1,55 @@
-'use strict'
+"use strict"
 
-let userInput = document.getElementById('user-input')
-let translatedPara = document.getElementById('pig-latin')
+let userInput = document.getElementById("user-input")
+let translatedPara = document.getElementById("pig-latin")
 
-let translateBtn = document.getElementById('translate')
+let translateBtn = document.getElementById("translate")
 
-let text = ''
-const vowels = ['a', 'e', 'i', 'o', 'u']
+translateBtn.addEventListener("click", () => {
+  let text = userInput.value
+  translatedPara.innerHTML = pigLatin(text)
+})
 
-translateBtn.addEventListener('click', () => {
-  text = userInput.value
-  let translated = ''
+//split sentence at spaces
+//from words array, convert each word  to pig latin
 
+function pigLatin(sentence) {
+  let translated = ""
+  let wordsArr = sentence.split(" ")
+
+  wordsArr.map((word) => {
+    translated += transformEachWord(word) + " "
+  })
+
+  return translated
+}
+
+function transformEachWord(word) {
+  const vowels = ["a", "e", "i", "o", "u"]
+  let translatedWord = ""
   // Rule 2: Words that begin with a vowel sound
-  if(vowels.includes(text.charAt(0))) {
-    return translatedPara.innerHTML = `${text}way`
+  if (vowels.includes(word.charAt(0).toLowerCase())) {
+    translatedWord = `${word}way`
   }
 
   // Rule 1: Words that start with a consonant cluster
   // loop through the word until vowel is encountered slice from the vowel till the end and slice from start until the vowel
   //then add 'ay' to the end
-  for(let i = 0; i < text.length; i++) {
-    if(vowels.includes(text.charAt(i))) {
-      translated = `${text.slice(i)}${text.slice(0, i)}ay`
-      i = text.length
+  else {
+    for (let i = 0; i < word.length; i++) {
+      if (vowels.includes(word.charAt(i))) {
+        translatedWord = `${word.slice(i)}${word.slice(0, i)}ay`
+        i = word.length
+      }
     }
   }
 
   // Rule 3: Maintain the capitalization after it has been converted to pig-latin
-  if(text.charAt(0) == text.charAt(0).toUpperCase()) {
-    translated = `${translated.charAt(0).toUpperCase()}${translated.slice(1).toLowerCase()}`
+  if (word.charAt(0) == word.charAt(0).toUpperCase()) {
+    translatedWord = `${translatedWord.charAt(0).toUpperCase()}${translatedWord
+      .slice(1)
+      .toLowerCase()}`
   }
 
-  translatedPara.innerHTML = translated
-})
+  return translatedWord
+}
